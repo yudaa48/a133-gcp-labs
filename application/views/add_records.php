8e829a612534143196fe97993ce3cb96ac202306
yudaa48 <yudaa0110@gmail.com>
2022-06-07 15:39:06 +0000

    fixing FE money tracker config, model and views

--- application/views/add_records.php
+++ application/views/add_records.php
@@ -13,25 +13,25 @@
     <title>Dashboard</title>
 
     <!-- Fontfaces CSS-->
-    <link href="<?php echo base_url(); ?>assets/css/font-face.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/css/font-face.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
 
     <!-- Bootstrap CSS-->
-    <link href="<?php echo base_url(); ?>assets/vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
 
     <!-- Vendor CSS-->
-    <link href="<?php echo base_url(); ?>assets/vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/wow/animate.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/slick/slick.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/select2/select2.min.css" rel="stylesheet" media="all">
-    <link href="<?php echo base_url(); ?>assets/vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/wow/animate.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/slick/slick.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
 
     <!-- Main CSS-->
-    <link href="<?php echo base_url(); ?>assets/css/theme.css" rel="stylesheet" media="all">
+    <link href="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/css/theme.css" rel="stylesheet" media="all">
 
 </head>
 
@@ -43,7 +43,7 @@
                 <div class="container-fluid">
                     <div class="header-mobile-inner">
                         <a class="logo" href="index.html">
-                            <img src="<?php echo base_url(); ?>assets/images/icon/logo.png" alt="CoolAdmin" />
+                            <img src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/images/icon/logo.png" alt="CoolAdmin" />
                         </a>
                         <button class="hamburger hamburger--slider" type="button">
                             <span class="hamburger-box">
@@ -75,7 +75,7 @@
         <aside class="menu-sidebar d-none d-lg-block">
             <div class="logo">
                 <a href="#">
-                    <img src="<?php echo base_url(); ?>assets/images/icon/logo.png" alt="Cool Admin" />
+                    <img src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/images/icon/logo.png" alt="Cool Admin" />
                 </a>
             </div>
             <div class="menu-sidebar__content js-scrollbar1">
@@ -190,28 +190,28 @@
     </div>
 
     <!-- Jquery JS-->
-    <script src="<?php echo base_url(); ?>assets/vendor/jquery-3.2.1.min.js"></script>
+    <script src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/jquery-3.2.1.min.js"></script>
     <!-- Bootstrap JS-->
-    <script src="<?php echo base_url(); ?>assets/vendor/bootstrap-4.1/popper.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <script src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/bootstrap-4.1/bootstrap.min.js"></script>
     <!-- Vendor JS       -->
-    <script src="<?php echo base_url(); ?>assets/vendor/slick/slick.min.js">
+    <script src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/slick/slick.min.js">
     </script>
-    <script src="<?php echo base_url(); ?>assets/vendor/wow/wow.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/animsition/animsition.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    <script src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/wow/wow.min.js"></script>
+    <script src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/animsition/animsition.min.js"></script>
+    <script src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
     </script>
-    <script src="<?php echo base_url(); ?>assets/vendor/counter-up/jquery.waypoints.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/counter-up/jquery.counterup.min.js">
+    <script src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/counter-up/jquery.counterup.min.js">
     </script>
-    <script src="<?php echo base_url(); ?>assets/vendor/circle-progress/circle-progress.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/chartjs/Chart.bundle.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/select2/select2.min.js">
+    <script src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/chartjs/Chart.bundle.min.js"></script>
+    <script src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/vendor/select2/select2.min.js">
     </script>
 
     <!-- Main JS-->
-    <script src="<?php echo base_url(); ?>assets/js/main.js"></script>
+    <script src="https://storage.googleapis.com/fe-assets-subsmission-yudaduy22/assets/js/main.js"></script>
 
 </body>
 
